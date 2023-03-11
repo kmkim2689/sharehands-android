4b96f37bce0086ab8dbbb427d61cd1a283b8dfbd
Kim Gimun <kmkim7575@gmail.com>
2023-03-12 00:44:40 +0900

    [ADD] 카카오 로그인 및 구글 소셜 로그인 환경 구축

--- app/src/main/java/com/sharehands/sharehands_frontend/MainActivity.kt
+++ app/src/main/java/com/sharehands/sharehands_frontend/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.sharehands.sharehands_frontend
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(ContentValues.TAG, "keyhash:${Utility.getKeyHash(this)}")
     }
 }
