adad5ff9542f07c603a308fd52f2a1687084f2e9
Kim Gimun <kmkim7575@gmail.com>
2023-03-30 09:24:18 +0900

    [ADD] 지원자 확인/모집 레이아웃 추가

---
+++ app/src/main/java/com/sharehands/sharehands_frontend/view/search/RecruitActivity.kt
@@ -0,0 +1,10 @@
+package com.sharehands.sharehands_frontend.view.search
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecruitActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
