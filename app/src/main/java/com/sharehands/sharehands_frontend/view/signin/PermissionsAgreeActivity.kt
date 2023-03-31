ad024e360155bd051b1ab0aacc2b84cbda413e33
Kim Gimun <kmkim7575@gmail.com>
2023-03-31 11:23:46 +0900

    [FEAT] 회원가입 약관동의 및 권한설정 구현

---
+++ app/src/main/java/com/sharehands/sharehands_frontend/view/signin/PermissionsAgreeActivity.kt
@@ -0,0 +1,76 @@
+package com.sharehands.sharehands_frontend.view.signin
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build.VERSION_CODES.P
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.sharehands.sharehands_frontend.R
+import com.sharehands.sharehands_frontend.databinding.ActivityPermissionsAgreeBinding
+
+class PermissionsAgreeActivity: AppCompatActivity() {
+    // 여기 안의 변수들은 전역적으로 사용할 것임.
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 200
+        private val REQUEST_CODES = arrayOf(CAMERA_PERMISSION_REQUEST_CODE, LOCATION_PERMISSION_REQUEST_CODE)
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+    lateinit var binding: ActivityPermissionsAgreeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permissions_agree)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_permissions_agree)
+
+        requestCameraPermission()
+        requestLocationPermission()
+
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Location permission granted, do something
+                } else {
+                    // Location permission denied, show message or handle accordingly
+                }
+            }
+
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Location permission granted, do something
+                } else {
+                    // Location permission denied, show message or handle accordingly
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+        } else {
+            // Permission already granted, do something
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            // Permission already granted, do something
+        }
+    }
+}
--- app/src/main/java/com/sharehands/sharehands_frontend/view/signin/TermsAgreeActivity.kt
+++ app/src/main/java/com/sharehands/sharehands_frontend/view/signin/TermsAgreeActivity.kt
@@ -1,10 +1,14 @@
 package com.sharehands.sharehands_frontend.view.signin
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.sharehands.sharehands_frontend.R
@@ -17,6 +21,7 @@ class TermsAgreeActivity: AppCompatActivity() {
         setContentView(R.layout.activity_terms_agree)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_terms_agree)
 
+        // UI 작업
         val titleText1 = binding.tvAgreeSubtitle
         val titleText2 = binding.tvAgreeTitle
         val titleText3 = binding.tvCheckInfo
@@ -24,10 +29,17 @@ class TermsAgreeActivity: AppCompatActivity() {
         val checkbox2 = binding.checkboxTerm2Agree
         val checkbox3 = binding.checkboxTerm3Agree
         val checkbox4 = binding.checkboxTerm4Agree
+        val checkbox5 = binding.checkboxEntireAgree
         val term1 = binding.tvTerm1Agree
         val term2 = binding.tvTerm2Agree
         val term3 = binding.tvTerm3Agree
         val term4 = binding.tvTerm4Agree
+        val termsEntire = binding.layoutEntireAgree
+        val nextBtnLayout = binding.layoutBtnNext
+        val nextBtnActive = binding.btnNextActive
+        val nextBtnInactive = binding.btnNextInactive
+        val nextBtnList: List<TextView> = listOf(nextBtnActive, nextBtnInactive)
+        val checkboxList: List<CheckBox> = listOf(checkbox1, checkbox2, checkbox3, checkbox4, checkbox5)
 
 
         titleText1.visibility = View.INVISIBLE
@@ -41,95 +53,156 @@ class TermsAgreeActivity: AppCompatActivity() {
         term3.visibility = View.INVISIBLE
         checkbox4.visibility = View.INVISIBLE
         term4.visibility = View.INVISIBLE
+        termsEntire.visibility = View.INVISIBLE
+        nextBtnLayout.visibility = View.INVISIBLE
+        nextBtnActive.visibility = View.INVISIBLE
+        nextBtnInactive.visibility = View.INVISIBLE
 
         Handler(Looper.getMainLooper()).postDelayed({
-            titleText1.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
-            titleText2.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
-            titleText3.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
+            appear(titleText1)
+            appear(titleText2)
+            appear(titleText3)
         }, 500)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            checkbox1.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
-            term1.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
+            appear(checkbox1)
+            appear(term1)
         }, 1000)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            checkbox2.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
-            term2.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
+            appear(checkbox2)
+            appear(term2)
         }, 1500)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            checkbox3.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
-            term3.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
+            appear(checkbox3)
+            appear(term3)
         }, 2000)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            checkbox4.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
-            term4.apply {
-                visibility = View.VISIBLE
-                startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_element_fade_in))
-            }
+            appear(checkbox4)
+            appear(term4)
         }, 2500)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            appear(termsEntire)
+        }, 3000)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            appear(nextBtnLayout)
+            appear(nextBtnInactive)
+        }, 3500)
+
         checkbox1.setOnClickListener {
-            if (checkbox1.isChecked) {
-                it.startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_checkbox))
-            } else {
-                it.clearAnimation()
-            }
+            checkTerm(checkbox1, it, checkboxList, nextBtnList)
         }
 
         checkbox2.setOnClickListener {
-            if (checkbox2.isChecked) {
-                it.startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_checkbox))
-            } else {
-                it.clearAnimation()
-            }
+            checkTerm(checkbox2, it, checkboxList, nextBtnList)
         }
 
         checkbox3.setOnClickListener {
-            if (checkbox3.isChecked) {
-                it.startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_checkbox))
-            } else {
-                it.clearAnimation()
-            }
+            checkTerm(checkbox3, it, checkboxList, nextBtnList)
         }
 
         checkbox4.setOnClickListener {
-            if (checkbox4.isChecked) {
-                it.startAnimation(AnimationUtils.loadAnimation(this@TermsAgreeActivity, R.anim.anim_checkbox))
-            } else {
-                it.clearAnimation()
+            checkTerm(checkbox4, it, checkboxList, nextBtnList)
+        }
+
+        checkbox5.setOnClickListener {
+            checkEntireTerms(checkboxList, it, nextBtnList)
+        }
+
+        binding.btnNextInactive.setOnClickListener {
+            binding.tvWarning.visibility = View.VISIBLE
+            binding.tvWarning.startAnimation(
+                AnimationUtils.loadAnimation(
+                    this@TermsAgreeActivity,
+                    R.anim.anim_element_fade_in
+                )
+            )
+        }
+
+        binding.btnNextActive.setOnClickListener {
+            val intent = Intent(this, PermissionsAgreeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun appear(titleText1: View) {
+        titleText1.apply {
+            visibility = View.VISIBLE
+            startAnimation(
+                AnimationUtils.loadAnimation(
+                    this@TermsAgreeActivity,
+                    R.anim.anim_element_fade_in
+                )
+            )
+        }
+    }
+
+    private fun checkEntireTerms(checkboxList: List<CheckBox>, it: View, nextBtnList: List<TextView>) {
+        if (checkboxList[4].isChecked) {
+            checkboxList[0].isChecked = true
+            checkboxList[1].isChecked = true
+            checkboxList[2].isChecked = true
+            checkboxList[3].isChecked = true
+            checkTerm(checkboxList[0], it, checkboxList, nextBtnList)
+            checkTerm(checkboxList[1], it, checkboxList, nextBtnList)
+            checkTerm(checkboxList[2], it, checkboxList, nextBtnList)
+            checkTerm(checkboxList[3], it, checkboxList, nextBtnList)
+            it.startAnimation(
+                AnimationUtils.loadAnimation(
+                    this@TermsAgreeActivity,
+                    R.anim.anim_checkbox
+                )
+            )
+        } else {
+            checkboxList[0].isChecked = false
+            checkboxList[1].isChecked = false
+            checkboxList[2].isChecked = false
+            checkboxList[3].isChecked = false
+            it.clearAnimation()
+        }
+    }
+
+    private fun checkTerm(checkbox: CheckBox, it: View, checkboxList: List<CheckBox>, nextBtnList: List<TextView>) {
+        if (checkbox.isChecked) {
+            it.startAnimation(
+                AnimationUtils.loadAnimation(
+                    this@TermsAgreeActivity,
+                    R.anim.anim_checkbox
+                )
+            )
+        } else {
+            it.clearAnimation()
+        }
+
+        // 4개가 모두 체크되면 전체 약관도 체크. 안그러면 체크 해제
+        checkboxList[4].isChecked = checkboxList[0].isChecked && checkboxList[1].isChecked && checkboxList[2].isChecked && checkboxList[3].isChecked
+
+        // 다음 버튼 활성화
+        if (checkboxList[0].isChecked && checkboxList[1].isChecked) {
+            nextBtnList[0].apply {
+                if (visibility == View.INVISIBLE) {
+                    visibility = View.VISIBLE
+                    startAnimation(
+                        AnimationUtils.loadAnimation(
+                            this@TermsAgreeActivity,
+                            R.anim.fade_in
+                        )
+                    )
+                }
+            }
+            nextBtnList[1].apply {
+                visibility = View.INVISIBLE
+            }
+        } else {
+            nextBtnList[0].apply {
+                visibility = View.INVISIBLE
+            }
+            nextBtnList[1].apply {
+                visibility = View.VISIBLE
             }
         }
     }
